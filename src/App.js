91a8febcaa2a79af18a163a8576594e00f783d72
Marcelo <81325245+MarceloRendon@users.noreply.github.com>
2024-01-24 16:48:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ReactComponent as IconGithub } from './assets/icons/github.svg';
 import { ReactComponent as IconLinkedin } from './assets/icons/linkedin.svg';
-import "./App.css";
 import ListItemText from '@mui/material/ListItemText';
 import TextField from '@mui/material/TextField';
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,6 +11,8 @@ class App extends React.Component {
     this.state = {
       codigoParadero: '',
       paraderos: [],
+      favoritos: [],
+      error: null,
     };
   }
 
@@ -18,35 +20,10 @@ class App extends React.Component {
     this.setState({ codigoParadero: event.target.value });
   }
 
-  // fetchData = () => {
-  //   const { codigoParadero, paraderos } = this.state;
-  //   fetch(`http://localhost:5005/paradero/${codigoParadero}`)
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       if (data.services && data.services.length > 0) {
-  //         const buses = data.services.reduce((allBuses, service) => {
-  //           if (service.buses && service.buses.length > 0) {
-  //             const busesWithServiceId = service.buses.map(bus => ({ ...bus, serviceId: service.id }));
-  //             return allBuses.concat(busesWithServiceId);
-  //           }
-  //           return allBuses;
-  //         }, []);
-  //         const newParadero = { codigoParadero, buses };
-  //         this.setState({ paraderos: [...paraderos, newParadero] });
-  //       } else {
-  //         this.setState({ paraderos: [...paraderos] });
-  //       }
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching data:', error);
-  //       this.setState({ paraderos: [...paraderos] });
-  //     });
-  // }
-
   fetchData = async () => {
     try {
       const { codigoParadero, paraderos } = this.state;
-      const response = await fetch(`http://localhost:5005/paradero/${codigoParadero}`);
+      const response = await fetch(`https://api.xor.cl/red/bus-stop/${codigoParadero}`);
       if (response.ok) {
         const data = await response.json();
         if (data.services && data.services.length > 0) {
@@ -60,7 +37,7 @@ class App extends React.Component {
           const newParadero = { codigoParadero, buses };
           this.setState({ paraderos: [...paraderos, newParadero], error: null });
         } else {
-          this.setState({ paraderos: [...paraderos], error: 'No hay información de servicios disponibles' });
+          this.setState({ error: 'No hay información de servicios disponibles' });
         }
       } else {
         throw new Error('Error al obtener información del paradero');
@@ -70,27 +47,53 @@ class App extends React.Component {
       this.setState({ error: 'Error interno del servidor' });
     }
   };
-  
+
+  guardarFavorito = async (paradero) => {
+    const { paraderos } = this.state;
+    const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    favoritos.push(paradero);
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+
+    // Realizar una nueva consulta a la API para obtener la información más reciente
+    try {
+      const response = await fetch(`https://api.xor.cl/red/bus-stop/${paradero.codigoParadero}`);
+      if (response.ok) {
+        const data = await response.json();
+        if (data.services && data.services.length > 0) {
+          const buses = data.services.reduce((allBuses, service) => {
+            if (service.buses && service.buses.length > 0) {
+              const busesWithServiceId = service.buses.map(bus => ({ ...bus, serviceId: service.id }));
+              return allBuses.concat(busesWithServiceId);
+            }
+            return allBuses;
+          }, []);
+          const updatedParadero = { codigoParadero: paradero.codigoParadero, buses };
+
+          // Actualizar la lista de paraderos en el estado
+          this.setState({ paraderos: [...paraderos, updatedParadero], favoritos, error: null });
+        } else {
+          this.setState({ error: 'No hay información de servicios disponibles' });
+        }
+      } else {
+        throw new Error('Error al obtener información del paradero');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      this.setState({ error: 'Error interno del servidor' });
+    }
+  };
+
+  verFavoritos = () => {
+    const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    this.setState({ favoritos });
+  };
 
   render() {
-    const { paraderos, error } = this.state;
+    const { paraderos, favoritos, error } = this.state;
     return (
       <div className="card">
-        <div className="header">
-          <div className="logo">
-            <a href=".">BustApp</a>
-          </div>
-          <div className="social">
-            <a href="#" title="Linkedin" target="_blank" rel="noopener noreferrer">
-              <IconLinkedin className="icon" />
-            </a>
-            <a href="https://github.com/MarceloRendon" title="GitHub" target="_blank" rel="noopener noreferrer">
-              <IconGithub className="icon" />
-            </a>
-          </div>
-        </div>
         <div className="content">
-        <div className="title-holder">
+          <div className="title-holder">
             <h1>¡Listo para zarpar hacia tu destino! Descubre todo sobre tu bus aquí.</h1>
             <p>Ingresa el código de tu paradero abajo para obtener información.</p>
           </div>
@@ -113,7 +116,7 @@ class App extends React.Component {
               }}
               sx={{
                 '& .filledInputRoot .MuiOutlinedInput-notchedOutline': {
-                  borderColor: 'black', // Color del borde del TextField
+                  borderColor: 'black',
                 },
               }}
               onChange={this.handleChange}
@@ -128,34 +131,69 @@ class App extends React.Component {
             </button>
           </div>
 
-          {error ? (
-            <div className="error-message">
-              <p>{error}</p>
-            </div>
-          ) : (
-            paraderos.map((paradero, index) => (
-              <div key={index} className="bus-info">
-                <h2>Paradero {paradero.codigoParadero}</h2>
-                {paradero.buses.length > 0 ? (
-                  paradero.buses.map((bus) => (
-                    <div key={bus.id} className="bus">
-                      <div className="service-id">{bus.serviceId}</div>
-                      <ListItemText
-                        primary={`Tiempo de Llegada: entre ${bus.min_arrival_time} y ${bus.max_arrival_time} min.`}
-                        secondary={`Distancia: ${bus.meters_distance} metros`}
-                      />
-                    </div>
-                  ))
-                ) : (
-                  <p>No hay información de buses disponible.</p>
-                )}
+          {/* Botón y sección para guardar favorito y ver favoritos */}
+          <div className="favoritos-section">
+            <button
+              className="favoritos-button"
+              onClick={this.verFavoritos}
+            >
+              Ver Favoritos
+            </button>
+            {error ? (
+              <div className="error-message">
+                <p>{error}</p>
               </div>
-            ))
-          )}
-        </div>
+            ) : (
+              favoritos.length > 0 && (
+                <div className="favoritos-list">
+                  <h2>Favoritos</h2>
+                  {favoritos.map((favorito, index) => (
+                    <div key={index} className="bus-info">
+                      <h2>Paradero {favorito.codigoParadero}</h2>
+                      {favorito.buses.length > 0 ? (
+                        favorito.buses.map((bus) => (
+                          <div key={bus.id} className="bus">
+                            <div className="service-id">{bus.serviceId}</div>
+                            <ListItemText
+                              primary={`Tiempo de Llegada: entre ${bus.min_arrival_time} y ${bus.max_arrival_time} min.`}
+                              secondary={`Distancia: ${bus.meters_distance} metros`}
+                            />
+                          </div>
+                        ))
+                      ) : (
+                        <p>No hay información de buses disponible.</p>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )
+            )}
+          </div>
 
-        <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/MarceloRendon" target="_blank" rel="noopener noreferrer">Marcelo Rendón</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/MarceloRendon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
+          {paraderos.map((paradero, index) => (
+            <div key={index} className="bus-info">
+              <h2>Paradero {paradero.codigoParadero}</h2>
+              {paradero.buses.length > 0 ? (
+                paradero.buses.map((bus) => (
+                  <div key={bus.id} className="bus">
+                    <div className="service-id">{bus.serviceId}</div>
+                    <ListItemText
+                      primary={`Tiempo de Llegada: entre ${bus.min_arrival_time} y ${bus.max_arrival_time} min.`}
+                      secondary={`Distancia: ${bus.meters_distance} metros`}
+                    />
+                  </div>
+                ))
+              ) : (
+                <p>No hay información de buses disponible.</p>
+              )}
+              <button
+                className="guardar-favorito-button"
+                onClick={() => this.guardarFavorito(paradero)}
+              >
+                Guardar como favorito
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     );
